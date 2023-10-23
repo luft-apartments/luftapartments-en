606b53fb1698355fa555340c82e94468dc1bcd32
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2023-10-23 22:26:33 +0200

    edit cookie info

--- components/CookieConsent/CookieConsent.js
+++ components/CookieConsent/CookieConsent.js
@@ -31,9 +31,9 @@ const CookieConsent = () => {
 
   return (
     <div className={styles.cookieBanner}>
-      <p>This website uses cookies to improve your experience.</p>
-      <button className={styles.buttonCookies} onClick={handleAccept}>Accept</button>
-      <button className={styles.buttonCookies} onClick={handleDecline}>Decline</button>
+      <p>Diese Website verwendet Cookies, um Ihr Erlebnis zu verbessern.</p>
+      <button className={styles.buttonCookies} onClick={handleAccept}>Akzeptieren</button>
+      <button className={styles.buttonCookies} onClick={handleDecline}>Abfall</button>
     </div>
   );
 };

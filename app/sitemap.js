54672a89bf9107c2900896a2fd30540453d04e9b
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2023-10-23 21:16:37 +0200

    generate sitemap xml

---
+++ app/sitemap.js
@@ -0,0 +1,43 @@
+export async function genetatePages() {
+  const params = {
+    query: `
+      query SiteMapQuery {
+        pages {
+          nodes {
+            date
+            seo {
+              opengraphUrl
+            }
+          }
+        }
+      }
+    `,
+  };
+
+  const response = await fetch(process.env.WP_GRAPHQL_URL, {
+    next: { revalidate: 60 },
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(params),
+  });
+
+  const { data } = await response.json();
+  const pagesData = data.pages.nodes;
+
+  return pagesData;
+}
+
+export default async function sitemap() {
+  const pagesData = await genetatePages();
+
+  const pages = pagesData.map(page => {
+    return {
+      url: page.seo.opengraphUrl, // Здесь используйте URL, который вы хотите добавить в карту сайта
+      lastModified: new Date(page.date), // Устанавливайте дату изменения страницы
+      changeFrequency: 'weekly', // Установите частоту изменения по своему усмотрению
+      priority: 0.8, // Установите приоритет по своему усмотрению
+    };
+  });
+
+  return pages;
+}

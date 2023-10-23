29c68ca3486b3595999df1064f17307cf4d92db7
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2023-10-23 20:25:40 +0200

    new trye sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,36 @@
-// /** @type {import('next-sitemap').IConfig} */
-// module.exports = {
-//   siteUrl: 'https://luftapartments.vercel.app/',
-//   generateRobotsTxt: true,
-// }
+const siteUrl = 'https://luftapartments.vercel.app';
+
+// add your private routes here
+const exclude = [
+  '/dashboard*',
+  '/settings*',
+  '/onboarding*',
+  '/blog/tags*',
+  '/auth*',
+];
+
+// Save crawling budget by not fetching SSG meta files
+const NEXT_SSG_FILES = [
+  '/*.json$',
+  '/*_buildManifest.js$',
+  '/*_middlewareManifest.js$',
+  '/*_ssgManifest.js$',
+  '/*.js$',
+];
+
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl,
+  generateRobotsTxt: true,
+  exclude,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        disallow: NEXT_SSG_FILES,
+      },
+    ],
+  },
+};
+
+export default config;

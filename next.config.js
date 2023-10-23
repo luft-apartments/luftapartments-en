ae5c8e26b728677cb629d16add88a8e1e95a430e
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2023-10-23 21:34:56 +0200

    add redirects

--- next.config.js
+++ next.config.js
@@ -4,6 +4,60 @@ const nextConfig = {
   images: {
     domains: [process.env.WP_IMAGES_URL],
   },
+  async redirects() {
+    return [
+      {
+        source: '/apartment_3a/',
+        destination: '/apartments/apartment-3a',
+        permanent: true,
+      },
+      {
+        source: '/apartment-2a/',
+        destination: '/apartments/apartment-2a',
+        permanent: true,
+      },
+      {
+        source: '/unser-apartment-1b/',
+        destination: '/apartments/apartment-1b',
+        permanent: true,
+      },
+      {
+        source: '/apartment-2b/',
+        destination: '/apartments/apartment-2b',
+        permanent: true,
+      },
+      {
+        source: '/apartment-3b/',
+        destination: '/apartments/apartment-3b',
+        permanent: true,
+      },
+      {
+        source: '/apartment_3a',
+        destination: '/apartments/apartment-3a',
+        permanent: true,
+      },
+      {
+        source: '/apartment-2a',
+        destination: '/apartments/apartment-2a',
+        permanent: true,
+      },
+      {
+        source: '/unser-apartment-1b',
+        destination: '/apartments/apartment-1b',
+        permanent: true,
+      },
+      {
+        source: '/apartment-2b',
+        destination: '/apartments/apartment-2b',
+        permanent: true,
+      },
+      {
+        source: '/apartment-3b',
+        destination: '/apartments/apartment-3b',
+        permanent: true,
+      },
+    ];
+  }
 };
 
 module.exports = nextConfig;

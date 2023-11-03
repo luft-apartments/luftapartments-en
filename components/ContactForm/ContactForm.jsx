b85f0cd06196ff884ac7eb9cf07806e2caa2d226
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2023-11-03 23:41:02 +0100

    ed onsubmit

--- components/ContactForm/ContactForm.jsx
+++ components/ContactForm/ContactForm.jsx
@@ -61,7 +61,7 @@ export const ContactForm = ({ onSubmitSuccess }) => {
   const onSubmit = async (values, { resetForm }) => {
     try {
       const { checkin, checkout, ...otherValues } = values; // Извлекаем checkin и checkout, оставляем остальные значения
-      await axios.post('/api/contact', values); // Отправляем данные формы на сервер
+      await axios.post('/api/contact', { ...otherValues, checkin, checkout }); // Отправляем данные формы на сервер
       // Здесь вы можете добавить код для обработки успешной отправки, например, очистка формы или вывод сообщения пользователю
       console.log('Форма успешно отправлена!');
       resetForm(); // Сбрасываем значения полей формы к исходным значениям
